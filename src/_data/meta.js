7b9bb5723d6d028a6fd8df77ab8120c86ee3a373
TomBatty <thomasgbatty@gmail.com>
2026-01-17 16:59:09 +1100

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,8 +2,8 @@ module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
   url: process.env.URL || "http://localhost:8080",
-  siteName: "Smol 11ty Starter",
+  siteName: "Wildwood Forest School",
   siteDescription:
-    "Extremely minimal Eleventy starter to kickstart a simple multi-page site / a nearly opinionless foundation to continue building on.",
-  authorName: "Stephanie Eckles", // optional
+    "All the latest info for Wildwood Forest School",
+  authorName: "Wildwood Forest School", // optional
 };
